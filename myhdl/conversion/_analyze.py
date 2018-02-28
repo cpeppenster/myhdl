da3385211b793c3b647d552e068c5c17bc1fe78f
Christiaan Peppenster <christiaan.peppenster@hudl.com>
2018-02-28 13:36:30 -0600

    Fix for myhdl issue #2

--- myhdl/conversion/_analyze.py
+++ myhdl/conversion/_analyze.py
@@ -124,6 +124,8 @@ def _analyzeSigs(hierarchy, hdl='Verilog'):
         for i, s in enumerate(m.mem):
             s._name = "%s%s%s%s" % (m.name, open, i, close)
             s._used = False
+            for sl in s._slicesigs:
+                sl._setName(hdl)
             if s._inList:
                 raise ConversionError(_error.SignalInMultipleLists, s._name)
             s._inList = True
